2b3aaa559e8c59245680cb3a4a5eede65b31a4e2
facu-glo <gabylope05@gmail.com>
2025-04-24 15:19:45 -0300

    configuracion markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -4,5 +4,12 @@ return {
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
-    opts = {},
+    config = function()
+        require('render-markdown').setup({
+            completions = {
+                blink = { enabled = true },
+                lsp = { enabled = false },
+            },
+        })
+    end
 }
