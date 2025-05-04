5a2753448b2bb928b5bc24d00ce9be56346077d5
facu-glo <gabylope05@gmail.com>
2025-05-04 11:42:00 -0300

    cambios menores

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,7 +3,6 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
     config = function()
         require('render-markdown').setup({
             completions = {
