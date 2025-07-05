7200bb94a96af850fe2c8d1562b4184405eff6b3
Facu <gabylope05@gmail.com>
2025-07-05 19:10:23 -0300

    extraigo los atajos de teclado a otra funcion para no repetir codigo

---
+++ lua/plugins/config/lsp/on_attach.lua
@@ -0,0 +1,9 @@
+return function(client, bufnr)
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+    -- Navegación
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", bufopts, { desc = "Ir a definición" }))
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", bufopts, { desc = "Mostrar Documentación" }))
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation,
+        vim.tbl_extend("force", bufopts, { desc = "Ir a implementación" }))
+end
