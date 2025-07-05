c3e572e4a77cb2947fc3a87719f6baa07036b3f7
Facu <gabylope05@gmail.com>
2025-07-05 20:50:14 -0300

    style: documentacion hover con bordes

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -21,7 +21,18 @@ return {
             cmdline = {
                 view = "cmdline",
             },
-
+            lsp = {
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                },
+                hover = {
+                    enabled = true, -- Activa el hover de noice
+                },
+            },
+            presets = {
+                lsp_doc_border = true,
+            },
         })
     end,
     dependencies = {
