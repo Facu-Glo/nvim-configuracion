62adee5c59334e08ecabafad91618e3cc2904ff2
Facu <gabylope05@gmail.com>
2025-09-12 12:50:50 -0300

    fix: fold

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,24 +1,24 @@
 local nvim = vim.o
 
-nvim.number         = true
-nvim.relativenumber = true
-nvim.expandtab      = true
-nvim.tabstop        = 4
-nvim.shiftwidth     = 4
-nvim.softtabstop    = 4
-nvim.clipboard      = "unnamedplus"
-nvim.ignorecase     = true
-nvim.smartcase      = true
-nvim.undofile       = true
-nvim.spelllang      = "es,en"
-nvim.spell          = false
-nvim.cursorline     = true
-nvim.wrap           = false
-nvim.showmode       = false
-nvim.splitright     = true
-nvim.textwidth      = 100
-nvim.foldmethod     = "expr"
-nvim.foldexpr       = "nvim_treesitter#foldexpr()"
-nvim.foldlevel      = 99
+nvim.number           = true
+nvim.relativenumber   = true
+nvim.expandtab        = true
+nvim.tabstop          = 4
+nvim.shiftwidth       = 4
+nvim.softtabstop      = 4
+nvim.clipboard        = "unnamedplus"
+nvim.ignorecase       = true
+nvim.smartcase        = true
+nvim.undofile         = true
+nvim.spelllang        = "es,en"
+nvim.spell            = false
+nvim.cursorline       = true
+nvim.wrap             = false
+nvim.showmode         = false
+nvim.splitright       = true
+nvim.textwidth        = 100
+vim.wo.foldmethod     = "expr"
+vim.wo.foldexpr       = "nvim_treesitter#foldexpr()"
+vim.wo.foldlevel      = 99
 
 vim.cmd.colorscheme("tokyonight-night")
