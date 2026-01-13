62ef751da400238e5bd30888e08c14a31c9b568d
Facu <gabylope05@gmail.com>
2026-01-12 21:29:21 -0300

    fix:treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         config = function()
-            require("nvim-treesitter.configs").setup({
+            require("nvim-treesitter.config").setup({
                 ensure_installed = {
                     "bash",
                     "html",
