f52b045303f68be2fdaaa2b4032d5f25183d81d5
Facu <gabylope05@gmail.com>
2026-02-09 15:27:54 -0300

    feat: comando para copiar el contenido de todos los buffers

--- lua/config/usercommand.lua
+++ lua/config/usercommand.lua
@@ -2,3 +2,13 @@ vim.api.nvim_create_user_command("TabPick", function()
     vim.cmd("tabnew")
     require("snacks.picker").files()
 end, { desc = "Abrir archivo en nueva pestaña con snacks.picker.files" })
+
+vim.api.nvim_create_user_command("CopiarTodo", function()
+    vim.fn.setreg('+', '')
+
+    vim.cmd([[
+        silent bufdo if &buflisted | let @+ .= "--- ARCHIVO: " . expand('%:.') . " ---\n" . getline(1, '$')->join("\n") . "\n\n" | endif
+    ]])
+
+    print("¡Todos los buffers listados han sido copiados!")
+end, { desc = "Copia contenido de buffers con su ruta usando bufdo" })
