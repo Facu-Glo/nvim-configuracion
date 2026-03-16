40b7fb62c006c5b9d663c5fa67bc0abb9e690af9
Facu <gabylope05@gmail.com>
2026-03-16 10:54:24 -0300

    refactor: extraigo en funciones logica para usar como comandos

--- lua/config/usercommand.lua
+++ lua/config/usercommand.lua
@@ -1,11 +1,11 @@
 local create_command = vim.api.nvim_create_user_command
 
-create_command("TabPick", function()
+local function tab_pick()
     vim.cmd("tabnew")
     require("snacks.picker").files()
-end, { desc = "Abrir archivo en nueva pestaña con snacks.picker.files" })
+end
 
-create_command("CopiarTodo", function()
+local function copy_all_buffers()
     vim.fn.setreg('+', '')
 
     vim.cmd([[
@@ -13,9 +13,9 @@ create_command("CopiarTodo", function()
     ]])
 
     print("¡Todos los buffers listados han sido copiados!")
-end, { desc = "Copia contenido de buffers con su ruta usando bufdo" })
+end
 
-create_command("Format", function(args)
+local function format_code(args)
     local range = nil
     if args.count ~= -1 then
         local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
@@ -29,4 +29,25 @@ create_command("Format", function(args)
         timeout_ms = 500,
         range = range
     })
-end, { range = true, desc = "Formatear código con Conform" })
+end
+
+local function abrir_pdf(opts)
+    local archivo = opts.args
+    if not archivo:match("%.pdf$") then
+        archivo = archivo .. ".pdf"
+    end
+
+    local ruta_completa = vim.fn.expand(archivo)
+
+    if vim.fn.filereadable(ruta_completa) == 1 then
+        vim.fn.jobstart({ "xdg-open", ruta_completa }, { detach = true })
+        print("Abriendo: " .. archivo)
+    else
+        print("Error: El archivo '" .. archivo .. "' no existe.")
+    end
+end
+
+create_command("TabPick", tab_pick, { desc = "Abrir archivo en nueva pestaña con snacks.picker.files" })
+create_command("Format", format_code, { range = true, desc = "Formatear código con Conform" })
+create_command("CopyAll", copy_all_buffers, { desc = "Copia contenido de buffers con su ruta usando bufdo" })
+create_command('PDF', abrir_pdf, { nargs = 1, complete = "file" })
