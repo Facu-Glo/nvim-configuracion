5ec20addb9feb8511577cbbb00aae8604fa50d40
Facu <gabylope05@gmail.com>
2026-05-03 13:49:55 -0300

    tresitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,48 +1,32 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
         config = function()
-            local ts = require("nvim-treesitter")
+            local ts = require('nvim-treesitter')
+            ts.setup {
+                install_dir = vim.fn.stdpath('data') .. '/site'
+            }
 
-            ts.setup({
-                install_dir = vim.fn.stdpath("data") .. "/site",
-            })
+            local my_langs = {
+                "bash", "html", "javascript", "json", "java", "lua",
+                "markdown", "markdown_inline", "python", "query", "regex",
+                "tsx", "typescript", "vim", "yaml", "xml", "c", "rust", "latex",
+            }
 
-            ts.install({
-                "bash",
-                "html",
-                "javascript",
-                "json", "java", "lua",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query", "regex",
-                "tsx",
-                "typescript",
-                "vim",
-                "yaml",
-                "nasm",
-                "hyprlang",
-                "latex",
-                "xml",
-                "rasi",
-                "elixir",
-                "heex",
-                "eex"
-            })
+            ts.install(my_langs)
 
             vim.api.nvim_create_autocmd("FileType", {
-                pattern = {
-                    "bash", "html", "javascript", "json", "java", "lua", "python",
-                    "elixir", "heex", "eex", "typescript", "tsx", "yaml", "xml"
-                },
-                callback = function()
+                pattern = my_langs,
+                callback = function(args)
                     vim.treesitter.start()
 
                     vim.wo.foldmethod = 'expr'
                     vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                    vim.wo.foldlevel = 99
 
-                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
                 end,
             })
         end,
