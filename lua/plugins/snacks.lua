1a3fb0cfb05872c283ff7762659fe17e90b889b6
Facu <gabylope05@gmail.com>
2026-09-03 15:46:40 -0300

    chore: poner bordes a terminal flotante

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -211,4 +211,10 @@ vim.keymap.set("n", "<leader>e", function() Snacks.explorer() end, { desc = "Fil
 -- Terminal
 vim.keymap.set({ "n", "t" }, "<leader>tt", function() Snacks.terminal.toggle(nil, { cwd = vim.fn.getcwd() }) end,
     { desc = "Terminal Toggle" })
-vim.keymap.set({ "n", "t" }, "<leader>tf", function() Snacks.terminal("zsh") end, { desc = "Terminal Zsh" })
+vim.keymap.set({ "n", "t" }, "<leader>tf", function()
+    Snacks.terminal("zsh", {
+        win = {
+            border = "rounded",
+        },
+    })
+end, { desc = "Terminal Zsh flotante con borde" })
