d9e280da56e522dacbdc40e45c24b15c1e932a39
Facu <gabylope05@gmail.com>
2026-09-19 11:21:29 -0300

    chore: deshabilitar atajos del dashboard 'f' y 'g' por incompatibilidad con otros atajos

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -15,8 +15,8 @@ require("snacks").setup({
                 text = {
                     { "  Update[u]  ", hl = "Label" },
                     { "  Session[s]  ", hl = "@property" },
-                    { "  Find Files[f]  ", hl = "DiagnosticInfo" },
-                    { " 󰱼 Find Text[g]  ", hl = "DiagnosticHint" },
+                    -- { "  Find Files[f]  ", hl = "DiagnosticInfo" },
+                    -- { " 󰱼 Find Text[g]  ", hl = "DiagnosticHint" },
                     { "  Quit[q]  " },
                 },
             },
@@ -34,18 +34,18 @@ require("snacks").setup({
                     require("persistence").load()
                 end
             },
-            {
-                icon = "",
-                key = "f",
-                action = function() Snacks.picker.files() end,
-                hidden = true
-            },
-            {
-                icon = "",
-                key = "g",
-                action = function() Snacks.picker.grep() end,
-                hidden = true
-            },
+            -- {
+            --     icon = "",
+            --     key = "f",
+            --     action = function() Snacks.picker.files() end,
+            --     hidden = true
+            -- },
+            -- {
+            --     icon = "",
+            --     key = "g",
+            --     action = function() Snacks.picker.grep() end,
+            --     hidden = true
+            -- },
             {
                 icon = "",
                 key = "u",
