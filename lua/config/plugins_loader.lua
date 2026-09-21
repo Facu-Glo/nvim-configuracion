0b6667a7ad5fb8607e0d274f79bba5d5b43084ef
Facu <gabylope05@gmail.com>
2026-09-21 10:38:17 -0300

    chore: desactivar el plugin de live-server

--- lua/config/plugins_loader.lua
+++ lua/config/plugins_loader.lua
@@ -13,20 +13,20 @@ local plugins = {
     "trouble",
     "gitsigns",
     "codediff",
-    "live_server",
+    -- "live_server",
     "multicursor",
     "whichkeys",
     "fyler",
     "flash",
 }
 
-local plugin_dir = vim.fn.stdpath("config") .. "/lua/plugins"
-for _, file in ipairs(vim.fn.readdir(plugin_dir)) do
-    local module = file:gsub("%.lua$", "")
-    if file:match("%.lua$") and not vim.iter(plugins):any(function(p) return p == module end) then
-        vim.notify("Plugin sin registrar en plugins_loader: " .. module, vim.log.levels.WARN)
-    end
-end
+-- local plugin_dir = vim.fn.stdpath("config") .. "/lua/plugins"
+-- for _, file in ipairs(vim.fn.readdir(plugin_dir)) do
+--     local module = file:gsub("%.lua$", "")
+--     if file:match("%.lua$") and not vim.iter(plugins):any(function(p) return p == module end) then
+--         vim.notify("Plugin sin registrar en plugins_loader: " .. module, vim.log.levels.WARN)
+--     end
+-- end
 
 for _, plugin in ipairs(plugins) do
     require("plugins." .. plugin)
