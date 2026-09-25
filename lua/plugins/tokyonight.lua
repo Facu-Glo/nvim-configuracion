3f8f73f2c819105d430b1fa9d0fe6596d7dcadc8
Facu <gabylope05@gmail.com>
2026-09-24 22:54:22 -0300

    add: agregar colores personalizados para mini.tabline

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -13,6 +13,37 @@ require("tokyonight").setup({
     end,
     on_highlights = function(highlights, _)
         highlights.ColorColumn = { bg = "#2c2e34" }
+
+        highlights.MiniTablineFill = { bg = "NONE" }
+
+        highlights.MiniTablineCurrent = {
+            bg = "#3a4261",
+            fg = "#c0caf5",
+            underline = true,
+            sp = "#7aa2f7",
+            bold = true,
+        }
+
+        highlights.MiniTablineModifiedCurrent = {
+            bg = "#3a4261",
+            fg = "#5BCFB1",
+            underline = true,
+            sp = "#5BCFB1",
+            bold = true,
+        }
+
+        highlights.MiniTablineModified = {
+            fg = "#5BCFB1",
+            bg = "NONE",
+        }
+        highlights.MiniTablineModifiedVisible = {
+            fg = "#5BCFB1",
+            bg = "NONE",
+        }
+        highlights.MiniTablineModifiedHidden = {
+            fg = "#5BCFB1",
+            bg = "NONE",
+        }
     end,
 })
 
